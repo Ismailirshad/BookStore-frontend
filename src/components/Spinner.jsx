4d56641d0e335d9e28c6f7e6a4af4c5864000378
Ismailirshad <irshadsha164@gmail.com>
2025-10-08 09:00:37 +0530

    Initial frontend commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center">
+    <div className='animate-ping w-150 h-60  m-8 rounded-xl bg-sky-600'></div>
+    </div>
+  )
+}
+
+export default Spinner
